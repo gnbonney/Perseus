70c82ae0687845f3e05b6f701361172a4acadd61
Greg Bonney <gnbonney@gmail.com>
2026-02-28 10:18:40 -0600

    Remove Eclipse license headers; use minimal copyright notice

--- src/gnb/jalgol/compiler/antlr/package-info.java
+++ src/gnb/jalgol/compiler/antlr/package-info.java
@@ -1,15 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2017 Greg Bonney and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     Greg Bonney - initial design and implementation
- *******************************************************************************/
-/**
- * @author Greg Bonney
- *
- */
+// Copyright (c) 2017-2026 Greg Bonney
+
 package gnb.jalgol.compiler.antlr;
--- test/gnb/jalgol/compiler/AntlrAlgolListenerTest.java
+++ test/gnb/jalgol/compiler/AntlrAlgolListenerTest.java
@@ -1,13 +1,5 @@
-/*******************************************************************************
- * Copyright (c) 2017 Greg Bonney and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     Greg Bonney - initial design and implementation
- *******************************************************************************/
+// Copyright (c) 2017-2026 Greg Bonney
+
 package gnb.jalgol.compiler;
 
 import static org.junit.Assert.*;
